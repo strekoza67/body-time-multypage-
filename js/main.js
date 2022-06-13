d835890e28de0c7bec3d6ac181a21fe1dc6018ad
unknown <strekoza_nrv67@mail.ru>
2022-06-13 21:36:04 +0500

    footer

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ for (let i = 0; i < slides.length; i++) {
     height: 16px;
     width: 16px;
     border-radius: 50%;
+    cursor: pointer;
   `;
 
   if (i == 0) {
@@ -85,5 +86,75 @@ prev.addEventListener('click', () => {
 
   dots.forEach(dot => dot.style.background = '#AEACAA');
   dots[sliderIndex - 1].style.background = '#363431';
-
 });
+
+dots.forEach(dot => {
+  dot.addEventListener('click', (e) => {
+    const slideTo = e.target.getAttribute('data-slide-to');
+
+    sliderIndex = slideTo;
+    offset = deleteNotDigits(width) * (slideTo - 1);
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    dots.forEach(dot => dot.style.background = '#AEACAA');
+    dots[sliderIndex - 1].style.background = '#363431';
+  });
+});
+
+// Modals
+function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+  let trigger = document.querySelectorAll(triggerSelector),
+    modal = document.querySelector(modalSelector),
+    close = document.querySelector(closeSelector),
+    window = document.querySelector('[data-modal]'),
+    scroll = calcScroll();
+
+  trigger.forEach(item => {
+    item.addEventListener('click', (e) => {
+      if (e.target) {
+        e.preventDefault();
+      }
+
+      window.style.display = 'none';
+      modal.style.display = 'block';
+      document.body.style.overflow = 'hidden';
+      document.body.style.marginRight = `${scroll}px`;
+    });
+
+    close.addEventListener('click', (e) => {
+      window.style.display = 'none';
+      modal.style.display = 'none';
+      document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        window.style.display = 'none';
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
+      }
+    });
+  });
+}
+
+function calcScroll() {
+  let div = document.createElement('div');
+
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.overflowY = 'scroll';
+  div.style.visibility = 'hidden';
+
+  document.body.appendChild(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+
+  return scrollWidth;
+}
+
+
+
+bindModal('.first__btn', '.popup', '.popup__close');
